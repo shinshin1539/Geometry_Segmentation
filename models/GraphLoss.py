a99de29ddbfe3e500ab6791819445848961077ef
Kakeru Ito <twinskake03030402@gmail.com>
2025-08-01 13:49:49 +0900

    modified

--- models/GraphLoss.py
+++ models/GraphLoss.py
@@ -1,64 +1,70 @@
-from pytorch3d.loss import chamfer_distance, mesh_laplacian_smoothing, mesh_edge_loss, mesh_normal_consistency
+# GraphLoss.py
+from pytorch3d.loss import (
+    chamfer_distance,
+    mesh_laplacian_smoothing,
+    mesh_edge_loss,
+    mesh_normal_consistency,
+)
 from pytorch3d.ops import sample_points_from_meshes
 from pytorch3d.structures import Meshes
+import torch
 import torch.nn as nn
 
 
 class MeshLoss(nn.Module):
-    def __init__(self, weight_chamfer=1.0, num_samples=5000, weight_edge=1.0, weight_norm=0.1, weight_lapa=0.1):
-        super(MeshLoss, self).__init__()
-        self.weight_chamfer = weight_chamfer
-        self.weight_edge = weight_edge
-        self.weight_norm = weight_norm
-        self.weight_lapa = weight_lapa
+    def __init__(
+        self,
+        *,
+        weight_chamfer=1.0,
+        num_samples=5000,
+        weight_edge=1.0,
+        weight_norm=0.1,
+        weight_lapa=0.1,
+    ):
+        super().__init__()
+        self.num_samples = num_samples
+        self.wc = weight_chamfer
+        self.we = weight_edge
+        self.wn = weight_norm
+        self.wl = weight_lapa
 
-    def forward(self, input, target, *args):
-        '''
-        :param inputs: [verts, faces]
-                    - in_verts (batch, num_point_{p}, 3)
-                    - faces    (batch, num_faces_{p}, 3)
-        :param target: ground truth verts (num_point_{t}, 3)
-        :return:
-        '''
-        in_verts, faces = input
+    def forward(self, input, target):
+        """
+        input  : [verts_b, faces_b]  (Tensor(Np,3), Tensor(Fp,3))
+        target : gt verts            (Tensor(Nt,3))
+        """
+        verts, faces = input
         gt_verts = target
 
-        mesh = Meshes(verts=in_verts, faces=faces)
+        mesh = Meshes(verts=[verts], faces=[faces])  # list 化が必須
+        pts_pred = sample_points_from_meshes(mesh, num_samples=self.num_samples)
 
-        chamfer = chamfer_distance(sample_points_from_meshes(mesh, num_samples=gt_verts.size()[1]), gt_verts)[0]
-        # chamfer = chamfer_distance(mesh.verts_padded(), gt_verts)[0]
-        chamfer = chamfer * self.weight_chamfer
-
-        lapa = mesh_laplacian_smoothing(mesh) * self.weight_lapa
-        edge = mesh_edge_loss(mesh) * self.weight_edge
-        norm = mesh_normal_consistency(mesh) * self.weight_norm
+        chamfer = chamfer_distance(pts_pred, gt_verts[None])[0] * self.wc
+        lapa    = mesh_laplacian_smoothing(mesh) * self.wl
+        edge    = mesh_edge_loss(mesh)           * self.we
+        norm    = mesh_normal_consistency(mesh)  * self.wn
         return chamfer, lapa, edge, norm
 
 
 class GraphLoss(nn.Module):
+    """
+    - p_verts / p_faces : 各スケールとも shape = (1,Np,3) / (1,Fp,3)
+    - gt_verts          : (Nt,3)  – 1 サンプル分のみ
+    """
 
-    def __init__(self, weight_chamfer=1.0, weight_edge=0.1, weight_norm=0.01, weight_lapa=0.1):
-        super(GraphLoss, self).__init__()
-        self.criterion = MeshLoss(weight_chamfer, weight_edge, weight_norm, weight_lapa)
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.criterion = MeshLoss(**kwargs)
 
-    def forward(self, inputs, target, *args):
-        _, p_verts, p_faces = inputs
-        _, verts = target
+    def forward(self, inputs, target):
+        _, p_verts_list, p_faces_list = inputs
+        _, gt_verts = target
 
-        chamfer_loss, edge_loss, norm_loss, lapa_loss = 0., 0., 0., 0.
-        for vert, face in zip(p_verts, p_faces):
-            chamfer, lapa, edge, norm = self.criterion([vert, face], verts)
+        chamfer = lapa = edge = norm = 0.0
+        for v, f in zip(p_verts_list, p_faces_list):
+            # squeeze(0) : バッチ次元を落とす → (Np,3)
+            c, l, e, n = self.criterion([v.squeeze(0), f.squeeze(0)], gt_verts)
+            chamfer += c; lapa += l; edge += e; norm += n
 
-            chamfer_loss += chamfer
-            edge_loss += edge
-            norm_loss += norm
-            lapa_loss += lapa
+        return chamfer + lapa + edge + norm
 
-        loss = chamfer_loss + lapa_loss + \
-               edge_loss + norm_loss
-
-        return loss
-
-
-def create_loss(weight=None, reduction='mean'):
-    return GraphLoss()
